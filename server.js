1d8ef57481bb3478a85fb03600a6851fc36e6984
malik.iavari <malik.iavari@centro.net>
2018-09-11 14:59:29 -0400

    Changed port to PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs'); 
 const fs = require('fs');
 
-const port = process.env.port || 3001;
+const port = process.env.PORT || 3001;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
